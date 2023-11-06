94907c87269e429609cde5d344fe0e0f72ddbdb4
DragonyteEvol <acuarionevol@outlook.com>
2023-11-06 13:17:46 -0500

    feat: Added alert warning for percetaje of kri that are more than 100

--- views/risk/show.php
+++ views/risk/show.php
@@ -3,6 +3,12 @@ include_once("components/header.php");
 ?>
 <div class="container">
 	<!-- ----------------------------------- -->
+	<?php if($data["score"]>=3): ?>
+	<div class="alert alert-warning alert-dismissible fade show my-3" role="alert">
+		<strong>Advertencia!</strong> Se recomienda revisar los porcentajes de kris para este riesgo 
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>
+	<?php  endif; ?>
 	<?php foreach($data["risks"] as $risk): ?>
 	<div class="row my-2">
 		<div class="col-12">
@@ -13,7 +19,7 @@ include_once("components/header.php");
 				<div class="col-6">
 					<div class="input-group">
 						<input class="form-control d-flex bg-white rounded-lg shadow" id="to" name="to" type="date">
-					<button onclick="redirect('risk',<?= $risk['master_id'] ?>,'from','to')" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787a5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0a4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501a6.921 6.921 0 0 0-1.315-.408a7.079 7.079 0 0 0-2.819 0a6.94 6.94 0 0 0-1.316.409a7.04 7.04 0 0 0-3.08 2.534a6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4l4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138a4.943 4.943 0 0 1-1.787.752a5.073 5.073 0 0 1-2.017 0a4.956 4.956 0 0 1-1.787-.752a5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0a7.031 7.031 0 0 0 4.395-2.945a6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4l-4 4z"/></svg></button>
+						<button onclick="redirect('risk',<?= $risk['master_id'] ?>,'from','to')" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M10 11H7.101l.001-.009a4.956 4.956 0 0 1 .752-1.787a5.054 5.054 0 0 1 2.2-1.811c.302-.128.617-.226.938-.291a5.078 5.078 0 0 1 2.018 0a4.978 4.978 0 0 1 2.525 1.361l1.416-1.412a7.036 7.036 0 0 0-2.224-1.501a6.921 6.921 0 0 0-1.315-.408a7.079 7.079 0 0 0-2.819 0a6.94 6.94 0 0 0-1.316.409a7.04 7.04 0 0 0-3.08 2.534a6.978 6.978 0 0 0-1.054 2.505c-.028.135-.043.273-.063.41H2l4 4l4-4zm4 2h2.899l-.001.008a4.976 4.976 0 0 1-2.103 3.138a4.943 4.943 0 0 1-1.787.752a5.073 5.073 0 0 1-2.017 0a4.956 4.956 0 0 1-1.787-.752a5.072 5.072 0 0 1-.74-.61L7.05 16.95a7.032 7.032 0 0 0 2.225 1.5c.424.18.867.317 1.315.408a7.07 7.07 0 0 0 2.818 0a7.031 7.031 0 0 0 4.395-2.945a6.974 6.974 0 0 0 1.053-2.503c.027-.135.043-.273.063-.41H22l-4-4l-4 4z"/></svg></button>
 					</div>
 				</div>
 			</div>
@@ -34,7 +40,7 @@ include_once("components/header.php");
 							<div class="h4 font-weight-bold mb-0"><?= $data['score'] ?> P.</div><span class="small text-gray">Puntaje calculado</span>
 						</div>
 						<div class="col-6">
-						<div class="h4 font-weight-bold mb-0"><?= $data["score_figured"]["value"] ?></div><span class="small text-gray"><?= $data["score_figured"]["qualifier"] ?></span>
+							<div class="h4 font-weight-bold mb-0"><?= $data["score_figured"]["value"] ?></div><span class="small text-gray"><?= $data["score_figured"]["qualifier"] ?></span>
 						</div>
 					</div>
 					<!-- END -->
@@ -123,7 +129,7 @@ include_once("components/header.php");
 						<button type="button" class="btn btn-primary">
 							<?= $kri['syntax_abstract'] ?>
 							<span class="badge text-bg-secondary">
-							<?= $kri['syntax'] ?>
+								<?= $kri['syntax'] ?>
 							</span>
 						</button>
 					</div>
