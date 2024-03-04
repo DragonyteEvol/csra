c0298c7be99215a55b65db3ff7a404f2c5afc6c5
DragonyteEvol <acuarionevol@outlook.com>
2024-03-03 20:39:46 -0500

    styles: styles for unexpected and unauthorized view

--- views/utils/unauthorized.php
+++ views/utils/unauthorized.php
@@ -1,2 +1,26 @@
-NO TIENES PERMISOS PARA ACCEDER A ESTE MODULO.
-CONTACTA CON UN ADMINISTRADOR BCRRO
+<?php  
+include_once("components/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="row my-5">
+			<div class="col-6 justify-content-between align-self-center ">
+				<h1>403</h1>
+				<p>Acceso denegado:</p>
+				<p>El usuario no tiene los permisos necesarios para acceder al modulo, contacte con su administrador para mas informaciòn</p>
+			</div>
+			<div class="col-6">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+</svg>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- views/utils/unexpected.php
+++ views/utils/unexpected.php
@@ -1,1 +1,26 @@
-HA OCURRIDO UN ERROR NO EXPERADO CONTACTA CON TU ADMINISTRADOR DE SOPHOS
+<?php  
+include_once("components/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="row my-5">
+			<div class="col-6 justify-content-between align-self-center ">
+				<h1>520</h1>
+				<p>Error inesperado:</p>
+				<p>Contacte con su administrador</p>
+			</div>
+			<div class="col-6">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+</svg>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
